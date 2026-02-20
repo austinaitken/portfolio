88ad7176fbca07a0df7e082026d04881c8120b6c
Austin Aitken <austinlaitken@gmail.com>
2026-02-19 17:04:06 -0800

    Added min-height to the animated name SVG to avoid collapsing on mobile

--- src/routes/Home/components/Contact/Contact.tsx
+++ src/routes/Home/components/Contact/Contact.tsx
@@ -1,9 +1,27 @@
 import styles from './Contact.module.css'
 
 const Contact = () => {
+    const handleEmailClick = () => {
+        window.location.href = 'mailto:austinlaitken@gmail.com'
+    }
+
+    const handleCallClick = () => {
+        window.location.href = 'tel:+17097703770'
+    }
+
     return (
         <div className={styles.container}>
-            <h1>Reach Out Today</h1>
+            <h1 className={styles.title}>
+                Reach Out Today
+            </h1>
+            <div className={styles.buttonsContainer}>
+                <button onClick={handleEmailClick}>
+                    Email
+                </button>
+                <button onClick={handleCallClick}>
+                    Call
+                </button>
+            </div>
         </div>
     )
 }

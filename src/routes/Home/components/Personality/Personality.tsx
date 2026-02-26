f6495ec212deb5fd677c1a8be9f0c3a7a293b181
Austin Aitken <austinlaitken@gmail.com>
2026-02-26 14:11:40 -0800

    Corrected HL7 FHIR image asset name

--- src/routes/Home/components/Personality/Personality.tsx
+++ src/routes/Home/components/Personality/Personality.tsx
@@ -9,9 +9,9 @@ const Personality = () => {
     return (
         <section className={styles.container} ref={personalityRef}>
             <h1>A Light in the Dark</h1>
-            <p>Tagline</p>
-            <p>This is a very long description about the personality traits and characteristics.</p>
-            <YouTubeVideo videoId="fppS6ddagRo" />
+            <div>
+                <YouTubeVideo videoId="fppS6ddagRo" />
+            </div>
             <YouTubeVideo videoId="a5QZmygubM8" />
         </section>
     )
--- src/routes/Home/components/Qualifications/Qualifications.tsx
+++ src/routes/Home/components/Qualifications/Qualifications.tsx
@@ -12,6 +12,10 @@ const Qualifications = () => {
             <h1>A Qualified Problem Solver</h1>
             <h2>Bachelor of Engineering - (Computer Engineering)</h2>
             <h2>Excellence Awarded.</h2>
+            <p>
+                Earned the Director's Award for Exemplary Work Terms by achieving an Outstanding
+                with Distinction on all 5 engineering work terms. A rare and prestigious award.{' '}
+            </p>
             <img src={munLogo} alt="Memorial University of Newfoundland Logo" />
             <img src={graduationCard} alt="Austin Aitken's Graduation Trading Card" />
         </section>

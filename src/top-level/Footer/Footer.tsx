12e5fb1c0fbdf6625970931e140840d9116bde1a
Austin Aitken <austinlaitken@gmail.com>
2026-03-16 20:28:01 -0700

    Trying to fix nav menu on mobile (bottom was hidden by the browser menu)

--- src/top-level/Footer/Footer.tsx
+++ src/top-level/Footer/Footer.tsx
@@ -1,3 +1,9 @@
-const Footer = () => <footer>Copyright &copy; {new Date().getFullYear()} Austin Aitken</footer>
+import styles from './Footer.module.css'
+
+const Footer = () => (
+    <footer className={styles.footer}>
+        Copyright &copy; {new Date().getFullYear()} Austin Aitken
+    </footer>
+)
 
 export default Footer
